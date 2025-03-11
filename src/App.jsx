bcf96a7f2eadf401ea61b7272b980b43b0fc0d4b
yourangrang <163999418+yourangrang@users.noreply.github.com>
2025-03-11 22:29:02 +0900

    Add loading time

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {lazy, Suspense} from 'react'
-import { FiLoader } from "react-icons/fi";
+import {lazy} from 'react'
+
 
 
 import React from 'react'
@@ -9,9 +9,9 @@ const Game = lazy( () => import('./game') )
 
 const App = () => {
   return (
-    <Suspense fallback={ <div className='loading'>로딩중입니다<FiLoader className='FiLoader'/></div> }>
+    <>
       <Game />
-    </Suspense>
+    </>
   )
 }
 
--- src/game.jsx
+++ src/game.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import './game.css'
-import { cardImg } from '../src/data'
+import './game.css';
+import { cardImg } from '../src/data';
 import { GrGamepad } from "react-icons/gr";
+import { FiLoader } from "react-icons/fi";
 
 const cardIcon = [
-    {img: cardImg[1].img, matched: false},
-    {img: cardImg[2].img, matched: false},
-    {img: cardImg[3].img, matched: false},
-    {img: cardImg[4].img, matched: false},
-    {img: cardImg[5].img, matched: false},
-    {img: cardImg[6].img, matched: false},
-    {img: cardImg[7].img, matched: false},
-    {img: cardImg[8].img, matched: false},
+    { img: cardImg[1].img, matched: false },
+    { img: cardImg[2].img, matched: false },
+    { img: cardImg[3].img, matched: false },
+    { img: cardImg[4].img, matched: false },
+    { img: cardImg[5].img, matched: false },
+    { img: cardImg[6].img, matched: false },
+    { img: cardImg[7].img, matched: false },
+    { img: cardImg[8].img, matched: false },
 ];
 
 const Game = () => {
@@ -23,7 +24,7 @@ const Game = () => {
     const [choiceTwo, setChoiceTwo] = useState(null);
     const [disabled, setDisabled] = useState(false);
     const [gameWon, setGameWon] = useState(false);
-
+    const [loading, setLoading] = useState(true); 
 
     // 카드 섞기
     const shuffleCard = () => {
@@ -55,7 +56,7 @@ const Game = () => {
                 setTimeout(() => resetTurn(), 1000);
             }
         }
-    }, );
+    }, [choiceOne, choiceTwo]);
 
     // 턴 리셋
     const resetTurn = () => {
@@ -77,41 +78,63 @@ const Game = () => {
         }
     }, [card]);
 
+    // 로딩
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+        return () => clearTimeout(timer);
+        
+    }, []);
+   
+
     return (
         <div className="game">
-            <h1>카드 매칭게임<GrGamepad /></h1>
-            <button onClick={shuffleCard}>다시하기</button>
-            <div className="turn_wrap">
-                <p>턴: {turn}</p>
-            </div>
-            {gameWon && (
-                <motion.div
-                    className="win_message"
-                    initial={{ scale: 0 }}
-                    animate={{ scale: 1 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    🎉YOU WIN!
-                </motion.div>
+            {loading ? (
+                <div className="loading">
+                    <h1>게임 로딩 중 <FiLoader className='FiLoader'/></h1>
+                </div>
+            ) : (
+                <>
+                    <h1>카드 매칭게임<GrGamepad /></h1>
+                    <button onClick={shuffleCard}>다시하기</button>
+                    <div className="turn_wrap">
+                        <p>턴: {turn}</p>
+                    </div>
+                    {gameWon && (
+                        <motion.div
+                            className="win_message"
+                            initial={{ scale: 0 }}
+                            animate={{ scale: 1 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            🎉YOU WIN!
+                        </motion.div>
+                    )}
+                    <div className="card_grid">
+                        {card.map((card) => (
+                            <motion.div
+                                key={card.id}
+                                className={`card ${card.matched ? 'matched' : ''} `}
+                                onClick={() => !disabled && handleChoice(card)}
+                                initial={{ rotateY: 0 }}
+                                animate={{
+                                    rotateY: card === choiceOne || card === choiceTwo || card.matched ? 180 : 0,
+                                }}
+                                transition={{ duration: 0.5 }}
+                            >
+                                <div className="front">
+                                    {card === choiceOne || card === choiceTwo || card.matched ? (
+                                        <img src={card.img} alt="Card front" />
+                                    ) : (
+                                        <img src={cardImg[0].img} alt="Card back" style={{ width: '100%', height: '100%' }} />
+                                    )}
+                                </div>
+                            </motion.div>
+                        ))}
+                    </div>
+                </>
             )}
-            <div className="card_grid">
-                {card.map((card) => (
-                    <motion.div
-                        key={card.id}
-                        className={`card ${card.matched ? 'matched' : ''} `}
-                        onClick={() => !disabled && handleChoice(card)}
-                        initial={{ rotateY: 0 }}
-                        animate={{
-                            rotateY: card === choiceOne || card === choiceTwo || card.matched ? 180 : 0,
-                        }}
-                        transition={{ duration: 0.5 }}
-                    >
-                        <div className="front">
-                            {card === choiceOne || card === choiceTwo || card.matched ? (<img src={card.img} alt="Card front" />) : (<img src={cardImg[0].img} alt="Card back" style={{ width: '100%', height: `100%` }} /> )}
-                        </div>
-                    </motion.div>
-                ))}
-            </div>
         </div>
     );
 };
